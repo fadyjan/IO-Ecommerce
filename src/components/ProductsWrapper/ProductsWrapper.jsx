f60c74dcffa20d17be6d9940df3875d43cfb185d
fady jan <fady.jan95@gmail.com>
2023-10-12 21:38:27 +0300

    Enhance #8 LoadingShimmer Wrapper to have the same css that Product page have

--- src/components/ProductsWrapper/ProductsWrapper.jsx
+++ src/components/ProductsWrapper/ProductsWrapper.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Card from "../../components/Card/Card";
 import { renderShimmers } from "../../utils/helperFunctions";
 import "./ProductsWrapper.css";
-import useProductFilter from "../../hooks/useProductFilter"
+import useProductFilter from "../../hooks/useProductFilter";
 import SearchController from "../SearchController/SearchController";
 const ProductsWrapper = ({ AllProducts }) => {
-  const [displayProducts, filterProducts] = useProductFilter(AllProducts.allProducts);
+  const [displayProducts, filterProducts] = useProductFilter(
+    AllProducts.allProducts
+  );
 
   const onChangeHandler = (e) => {
     const SearchInput = e.target.value.toLowerCase();
@@ -14,11 +16,15 @@ const ProductsWrapper = ({ AllProducts }) => {
 
   return (
     <div id="AllProductsPage">
-      {AllProducts.loading && renderShimmers()}
-      {!AllProducts.loading && AllProducts.error && <h1>{AllProducts.error}</h1>}
+      {AllProducts.loading && (
+        <div className="LoadingShimmers">{renderShimmers()}</div>
+      )}
+      {!AllProducts.loading && AllProducts.error && (
+        <h1>{AllProducts.error}</h1>
+      )}
       {!AllProducts.loading && AllProducts.allProducts.length && (
         <>
-          <SearchController onChange={onChangeHandler}  />
+          <SearchController onChange={onChangeHandler} />
           <div className="CardsWarpper">
             {displayProducts.map((item) => (
               <Card key={item.id} item={item} />
