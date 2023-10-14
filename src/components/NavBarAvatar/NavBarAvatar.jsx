85eabc6d5e5279fb624baf087ae3bbb4d91e82b5
fady jan <fady.jan95@gmail.com>
2023-10-14 19:24:15 +0300

    Enhance #16 FeedBack message when the users is not loggedin and clicks on logout

--- src/components/NavBarAvatar/NavBarAvatar.jsx
+++ src/components/NavBarAvatar/NavBarAvatar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./NavBarAvatar.css";
 import AvatarrIcon from "../../assets/images/image-avatar.png";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { removeUserToken } from "../../store/ReduxSlices/AuthSlice";
 import { emptyCart } from "../../store/ReduxSlices/CartSlice";
 import ModalPopUp from "../ModalPopUp/ModalPopUp";
@@ -10,17 +10,28 @@ import useOnclickOutside from "react-cool-onclickoutside";
 const NavBarAvatar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [messageContent, setMessageContent] = useState("");
+  const isLogin = useSelector((state) => state.auth.userToken);
   const dipatch = useDispatch();
 
   const handleToggleDropdown = () => {
     setDropdownVisible((prevState) => !prevState);
   };
 
+  // Function that will be fire if the user already loggedin 
   const HandleLogOut = () => {
     dipatch(removeUserToken());
-    dipatch(emptyCart())
-    setIsModalOpen(true)
-    setDropdownVisible(false)
+    dipatch(emptyCart());
+    setMessageContent("Hope to see you soon");
+    setIsModalOpen(true);
+    setDropdownVisible(false);
+  };
+  
+  // Function that will be fire if the user not logged in 
+  const HandleShowMessage = () => {
+    setMessageContent("You have to log in first")
+    setIsModalOpen(true);
+    setDropdownVisible(false);
   };
   const ref = useOnclickOutside(() => {
     setDropdownVisible(false);
@@ -28,17 +39,19 @@ const NavBarAvatar = () => {
 
   return (
     <div onClick={handleToggleDropdown} ref={ref}>
-      <img id="AvatarIcon" src={AvatarrIcon} ></img>
+      <img id="AvatarIcon" src={AvatarrIcon}></img>
       {isDropdownVisible && (
-        <div id="avatarDropdown" >
+        <div id="avatarDropdown">
           <ul>
-            <li  onClick={HandleLogOut}>Log Out</li>
+            <li onClick={isLogin != "" ? HandleLogOut : HandleShowMessage}>
+              Log Out
+            </li>
           </ul>
         </div>
       )}
       {isModalOpen ? (
         <ModalPopUp
-          contentMessage={"Hope to see you soon"}
+          contentMessage={messageContent}
           onClose={() => setIsModalOpen(false)}
         ></ModalPopUp>
       ) : null}
