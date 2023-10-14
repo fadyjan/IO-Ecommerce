1bdc712488f0d8cb0da560f6c34cfc1771a96d32
fady jan <fady.jan95@gmail.com>
2023-10-14 19:08:45 +0300

    Enhance #14 Feedback message after the user cicks on Add To Cart Button inside product page

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addProduct } from "../../store/ReduxSlices/CartSlice";
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import ModalPopUp from "../ModalPopUp/ModalPopUp";
 const AddToCart = ({ amount, product }) => {
-  const navigate  = useNavigate ();
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const isUserLogedIn = useSelector((state) => state.auth.userToken);
 
+  //
   const handleAddingProdcutToCart = () => {
+    setShowModal(true);
     dispatch(addProduct({ ...product, amount }));
+    setTimeout(() => {
+      setShowModal(false);
+    }, 2000);
   };
+
   const redirectToLoginPage = () => {
-    navigate('/login');
+    setShowModal(true);
+    setTimeout(() => {
+      setShowModal(false);
+      navigate("/login");
+    }, 2500);
   };
+
   return (
-    <button
-      id="AddToCartButton"
-      onClick={
-        isUserLogedIn != "" ? handleAddingProdcutToCart : redirectToLoginPage
-      }
-    >
-      Add To Cart
-    </button>
+    <>
+      {showModal && (
+        <ModalPopUp
+          onClose={() => setShowModal(false)}
+          contentMessage={
+            isUserLogedIn
+              ? "Item has been added to the cart"
+              : "You have to log in first, redirect to login page"
+          }
+        />
+      )}
+      <button
+        id="AddToCartButton"
+        onClick={
+          isUserLogedIn != "" ? handleAddingProdcutToCart : redirectToLoginPage
+        }
+      >
+        Add To Cart
+      </button>
+    </>
   );
 };
 
